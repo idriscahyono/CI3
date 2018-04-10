f6185952fd37ac93ac91754a1258ab73e63d7018
idriscahyonoo <idriscahyono@gmail.com>
2018-04-10 23:15:20 +0700

    Update 10 April 2018 (Blog)

--- application/views/blog.php
+++ application/views/blog.php
@@ -73,7 +73,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <script src="assets/js/bootstrap.min.js"></script>
 </head>
 <body>
-
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -83,8 +82,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       <li><a href="<?php echo site_url('home');?>">Home</a></li>
       <li><a href="<?php echo site_url('about');?>">About</a></li>      
       <li><a href="<?php echo site_url('contact') ?>">Contact</a></li>
-      <li  class="active"><a href="<?php echo site_url('news') ?>">News</a></li>
-      <li><a href="<?php echo site_url('blog') ?>">Blog</a></li>
+      <li><a href="<?php echo site_url('news') ?>">News</a></li>
+      <li class="active"><a href="<?php echo site_url('blog') ?>">Blog</a></li>
       <li><a href="<?php echo site_url('crud') ?>">Tambah Blog</a></li>
     </ul>
     <div class="navbar-form navbar-left" action="/action_page.php">
@@ -106,21 +105,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   </div>
 </nav>
 	<div id="container">
-	<h1>News</h1>
-
-	<div class="colxs-12 col-sn-12 col-md-12 col-lg-12">
-		<?php for ($i=0; $i < $jumlahNews; $i++) { ?> 
-			Jumlah Berita <br>
-		<?php } ?>
-		
+		<h1>Blog</h1>
+		<?php 
+			foreach ($daftar_artikel->result() as $a) 
+			{
+				echo "<h3>".$a->title."</h3>
+				<p>Author: ".$a->author."</p>
+				<p>Date  : ".$a->date."<p>
+				<p>".$a->content."<p>
+				<p>".anchor('blog/read/'.$a->id,'Detail')."<p>";
+			}
+		 ?>
+		<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 	</div>
-
-	<!-- <div id="body">
-		<h2>Nama  : Idris Cahyono</h2>
-		<h2>Nim	  	: 1641720184 </h2>
-	</div> -->
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
 </body>
 </html>
